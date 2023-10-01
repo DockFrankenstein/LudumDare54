33e4d9a7b9bf297c9ead5d7cad96c3f06ebfbb09
DockFrankenstein <franciszekpyrc@gmail.com>
2023-10-01 13:58:34 +0200

    Added floating text

---
+++ LudumDare54/FloatingText.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Physics;
+using Stride.Engine;
+using qASIC;
+using Stride.Core;
+
+namespace LudumDare54
+{
+    public class FloatingText : SyncScript
+    {
+        public Player.PlayerLook look;
+
+        public override void Start()
+        {
+            base.Start();
+        }
+
+        public override void Update()
+        {
+            look.XRotation.WorldMatrix.DecomposeXYZ(out Vector3 rotation);
+            Entity.Transform.RotationEulerXYZ = rotation;
+        }
+    }
+}
